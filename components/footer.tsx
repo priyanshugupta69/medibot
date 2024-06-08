dc3666f99b928426bbc87e92b661c75c6a57c450
priyanshu <priyanshuraja456@gmail.com>
2024-06-08 15:41:01 +0530

    minor design fixes

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mt-8">
+    <footer>
       <div className="container mx-auto text-center text-xs">
         &copy; {new Date().getFullYear()} Medibot. Made with ❤️
       </div>
