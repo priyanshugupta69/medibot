35de7fdac3ea24bf09c280aad68620f62c0e06b1
priyanshu <priyanshuraja456@gmail.com>
2024-06-11 22:28:56 +0530

    tagline magnified

--- components/chatapp.tsx
+++ components/chatapp.tsx
@@ -102,7 +102,7 @@ const ChatApp: React.FC = () => {
                 <div className='flex items-center justify-start' style={{ alignItems: 'center', justifyContent: 'center' }}>
                     <img src="/human_2.png" alt="Medibot Logo" className="w-28 md:w-52" />
                 <div className="flex flex-col space-y-6"><div className='text-5xl md:text-8xl text-[#323557] font-serif'>Huego</div>
-                    <div className='flex justify-center text-xs p-1 font-semibold rounded-lg font-serif md:text-sm'>I&apos;m your personal AI-powered health assistant.</div>
+                    <div className='flex justify-center text-sm p-1 font-semibold rounded-lg font-serif'>I&apos;m your personal AI-powered health assistant.</div>
                 </div>
                 </div>
                 <div className='absolute text-center w-full'>
