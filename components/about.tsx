8ca507e684917a84ae9e26a250e1468789c2e385
Kaustubh Verma <vermakaustubh28@gmail.com>
2024-07-19 21:27:06 +0530

    name change

--- components/about.tsx
+++ components/about.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Footer from './footer';
+import React from "react";
+import Footer from "./footer";
 const About = () => {
   return (
     <div className="container mx-auto p-10 md:p-32 pt-20 lg:pt-32 py-10 text-justify">
@@ -7,26 +7,41 @@ const About = () => {
       <div className="space-y-6">
         <section>
           <p className="">
-            With Huego.Ai at your fingertips, let&apos;s disrupt the sick care system with the power of AI. Huego is powered by clinical vignettes, which typically involve a brief description of a patient&apos;s medical history, symptoms, and physical exam findings to illustrate a particular medical condition or situation. This can be useful in medical education and training, as well as in clinical assessments and decision-making.
+            With Huego.Ai at your fingertips, let&apos;s disrupt the sick care
+            system with the power of AI. Huego is powered by clinical vignettes,
+            which typically involve a brief description of a patient&apos;s
+            medical history, symptoms, and physical exam findings to illustrate
+            a particular medical condition or situation. This can be useful in
+            medical education and training, as well as in clinical assessments
+            and decision-making.
           </p>
         </section>
         <section>
           <p className="">
-            Clinical vignettes can come in different forms and may focus on various aspects of patient care, such as diagnostic evaluation, treatment options, management strategies, ethical considerations, or communication skills. They may also vary in complexity and scope, depending on the intended audience and purpose.
+            Clinical vignettes can come in different forms and may focus on
+            various aspects of patient care, such as diagnostic evaluation,
+            treatment options, management strategies, ethical considerations, or
+            communication skills. They may also vary in complexity and scope,
+            depending on the intended audience and purpose.
           </p>
         </section>
         <section>
           <p className="">
-            Overall, clinical vignettes can provide valuable insights and opportunities for learning and reflection in medical practice.
+            Overall, clinical vignettes can provide valuable insights and
+            opportunities for learning and reflection in medical practice.
           </p>
         </section>
         <section>
-          <p className='font-semibold'>
-            &ldquo;In every act of care, we sow the seeds of hope and compassion , let’s make access to healthcare ubiquitous.&rdquo; - <br />
+          <p className="font-semibold">
+            &ldquo;In every act of care, we sow the seeds of hope and compassion
+            , let’s make access to healthcare ubiquitous.&rdquo; - <br />
             {/* <p className='mt-2 font-normal'> */}
-            <span className='font-normal'>Dr. Tanner Ahmed</span><br />
-            <span className='font-normal'>Cofounder</span><br />
-            <span className='font-normal'>Huego Labs Inc</span><br />
+            <span className="font-normal">Dr. Tanner Khandaker</span>
+            <br />
+            <span className="font-normal">Cofounder</span>
+            <br />
+            <span className="font-normal">Huego Labs Inc</span>
+            <br />
             {/* </p> */}
           </p>
         </section>
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Footer from './footer';
+import React from "react";
+import Footer from "./footer";
 
 const ContactInfo = () => {
   return (
@@ -7,7 +7,11 @@ const ContactInfo = () => {
       <h1 className="text-4xl font-bold mb-4">Contact Information</h1>
       <div className="space-y-6">
         <section>
-          <h2 className="text-2xl font-semibold mb-6"><span>Huego.ai</span><br /> <span>Bumblebee Labs</span></h2>
+          <h2 className="text-2xl font-semibold mb-6">
+            <span>Dr. Tanner Khandaker</span>
+            <br /> <span>Cofounder</span>
+            <br /> <span>Huego Labs Inc</span>
+          </h2>
           <div className="mb-6">
             <h3 className="text-xl font-semibold mb-2">Address</h3>
             <p>440 North Wolfe Road</p>
