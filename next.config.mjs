5b96cca70d9bf8ad3f73957c958913b7aeedeb47
priyanshugupta69 <priyanshuraja456@gmail.com>
2026-05-12 16:24:00 +0530

    updated paths

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://medocgpt.duckdns.org/:path*',
+        destination: 'https://api.huego.resumatch.co.in/:path*',
       },
     ];
   },
